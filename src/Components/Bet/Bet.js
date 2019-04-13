9c5946e2f17bf5ce1e7ecda9ca8daf8305e0e291
Bryant <cabrera.bryant@gmail.com>
2019-04-13 00:19:51 -0700

    Bryant creates CSS grid layout for Bet.js component.

--- src/Components/Bet/Bet.js
+++ src/Components/Bet/Bet.js
@@ -4,7 +4,11 @@ import './Bet.css';
 export default class Bet extends Component {
     state = {
         teamAValue: 0.00,
-        teamBValue: 0.00
+        teamAPool: 0.00,
+        teamAMine: 0.00,
+        teamBValue: 0.00,
+        teamBPool: 0.00,
+        teamBMine: 0.00
     }
 
     handleChange = (e) => {
@@ -20,8 +24,8 @@ export default class Bet extends Component {
 
                 <div className="bet__teamA bet__team">
                     <h2 className="bet__team__title">TeamA</h2>
-                    <h3 className="bet__team__pool">Pool: $</h3>
-                    <h3 className="bet__team__mine">My Bet: $</h3>
+                    <h3 className="bet__team__pool">Pool: ${this.state.teamAPool}</h3>
+                    <h3 className="bet__team__mine">My Bet: ${this.state.teamAMine}</h3>
                     <div className="bet__team__input">
                         $<input type="number" name="teamAValue" value={this.state.teamAValue} onChange={this.handleChange}min="0.01" step="0.01" max="100.00"></input>
                     </div>
@@ -30,8 +34,8 @@ export default class Bet extends Component {
 
                 <div className="bet__teamB bet__team">
                     <h2 className="bet__team__title">TeamB</h2>
-                    <h3 className="bet__team__pool">Pool: $</h3>
-                    <h3 className="bet__team__mine">My Bet: $</h3>
+                    <h3 className="bet__team__pool">Pool: ${this.state.teamBPool}</h3>
+                    <h3 className="bet__team__mine">My Bet: ${this.state.teamBMine}</h3>
                     <div className="bet__team__input">
                         $<input type="number" name="teamAValue" value={this.state.teamBValue} onChange={this.handleChange}min="0.01" step="0.01" max="100.00"></input>
                     </div>

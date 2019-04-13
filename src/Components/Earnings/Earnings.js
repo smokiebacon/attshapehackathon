b8097b4853ebb681d0c4f1376a5c7fb8d33d27fa
Bryant <cabrera.bryant@gmail.com>
2019-04-13 11:58:57 -0700

    Bryant creates Earnings jsx in Earnings.js component.

--- src/Components/Earnings/Earnings.js
+++ src/Components/Earnings/Earnings.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import './Earnings.css';
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div className="earnings">
-                <h2>Earnings</h2>
+                <div className="earnings__earnings totals-box">
+                    <h2 className="totals-box__title">Earnings:</h2>
+
+                    <div className="totals-box__toggle">
+                        <h2>Week</h2>
+                        <h2>Month</h2>
+                        <h2>Total</h2>
+                    </div>
+
+                    <div className="totals-box__total">
+                        $3922.23
+                    </div>
+
+                    <button className="totals-box__btn">Cash Out</button>
+                </div>
+
             </div>
         );
     }

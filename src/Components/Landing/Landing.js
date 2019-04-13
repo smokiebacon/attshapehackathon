e71671790ad347de22e7026cd5c3d2ed3bdd6e5b
Abraham Hsu <smokiebacon@gmail.com>
2019-04-13 13:29:32 -0700

    abraham styled games and searchbar

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -3,16 +3,12 @@ import { Link } from 'react-router-dom'
 import './Landing.css'
 
 class Landing extends Component {
-    state = {
-        search: ''
-    }
-
-    doHandleInput = (e) => {
+  
+    changeHandler = (e) => {
         this.setState({
-            [e.target.name]:  e.target.value
-        })
-    }
-
+          [e.target.name] : e.target.value
+        });
+      }
     render() {
         return (
             <div className="dashboard__maincontainer">
@@ -27,17 +23,42 @@ class Landing extends Component {
                     <div className="dashboard__userprofile">
                         <input className="dashboard__userprofile--searchbar" name="search" placeholder="Search"></input>
                         <div className="dashboard__userprofile--pictures">
-                            <Link to='/stats'>
-                            
-                            <img className="dashboard__userprofile--pictures1" alt="" src="https://images.unsplash.com/photo-1551843073-4a9a5b6fcd5f?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" /></Link>
-                            <img className="dashboard__userprofile--pictures1" alt="" src="https://images.unsplash.com/photo-1551771825-1607dd0ad791?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
-                            <img className="dashboard__userprofile--pictures1" alt="" src="https://images.unsplash.com/photo-1551728339-499b333c5c43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
-                            <img className="dashboard__userprofile--pictures1" alt="" src="https://images.unsplash.com/photo-1551854386-b42759a60dd0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
-                            
-                            <img className="dashboard__userprofile--pictures1" alt="" src="https://images.unsplash.com/photo-1551833726-b6549cd73566?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
-                            <img className="dashboard__userprofile--pictures1" alt="" src="https://images.unsplash.com/photo-1551838030-f478473c9f0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
-                            <img className="dashboard__userprofile--pictures1" alt="" src="https://images.unsplash.com/photo-1483323323858-4916bde7bd5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
-
+                            <Link to='/live-match'>
+                                <img className="dashboard__userprofile--pictures1" alt="League of Legends" src="https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-188x250.jpg" />
+                                <h6>League of Legends</h6>
+                            </Link>
+                            <Link to='/fortnight'>
+                                <img className="dashboard__userprofile--pictures1" alt="Fortnite" src="https://static-cdn.jtvnw.net/ttv-boxart/Fortnite-188x250.jpg" />
+                            <h6>Fortnite</h6>
+                            </Link>
+                            <Link to ='/apexlegends'>
+                                <img className="dashboard__userprofile--pictures1" alt="Apex Legends" src="https://static-cdn.jtvnw.net/ttv-boxart/Apex%20Legends-188x250.jpg" />
+                                <h6>Apex Legends</h6>
+                            </Link>
+                            <Link to="/cs:go">
+                                <img className="dashboard__userprofile--pictures1" alt="Counter-Strike:Global Offensive" src="https://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-285x380.jpg" />
+                                <h6>Counter-Strike: Global Offensive</h6>
+                            </Link>
+                            <Link to ="/hearthstone">
+                                <img className="dashboard__userprofile--pictures1" alt="" src="https://static-cdn.jtvnw.net/ttv-boxart/Hearthstone-285x380.jpg" />
+                                <h6>Hearthstone</h6>
+                            </Link>
+                            <Link to ="/dota2">
+                                <img className="dashboard__userprofile--pictures1" alt="" src="https://static-cdn.jtvnw.net/ttv-boxart/Dota%202-285x380.jpg" />
+                                <h6>Dota 2</h6>
+                            </Link>
+                            <Link to ="/overwatch">
+                                <img className="dashboard__userprofile--pictures1" alt="" src="https://static-cdn.jtvnw.net/ttv-boxart/Overwatch-285x380.jpg" />
+                                <h6>Overwatch</h6>
+                            </Link>
+                            <Link to ="/starcraft2">
+                                <img className="dashboard__userprofile--pictures1" alt="" src="https://static-cdn.jtvnw.net/ttv-boxart/StarCraft%20II-285x380.jpg" />
+                                <h6>Starcraft 2</h6>
+                            </Link>
+                            <Link to ="/division2">
+                                <img className="dashboard__userprofile--pictures1" alt="" src="https://static-cdn.jtvnw.net/ttv-boxart/Tom%20Clancy%27s%20The%20Division%202-285x380.jpg" />
+                                <h6>Tom Clancy's Division 2</h6>
+                            </Link>
                         </div>
                     </div>
                 </div>
@@ -46,4 +67,4 @@ class Landing extends Component {
     }
 }
 
-export default Landing
+export default Landing;

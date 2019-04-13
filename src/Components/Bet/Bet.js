3595bc010c5b6123fd54cf33b0ec11c0196af9c6
Bryant <cabrera.bryant@gmail.com>
2019-04-12 23:49:22 -0700

    Bryant creates HTML scaffolding in Bet.js.

--- src/Components/Bet/Bet.js
+++ src/Components/Bet/Bet.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
 export default class Bet extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Bet</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="bet">
+                <h1 className="bet__title">Current Bets</h1>
+
+                <div className="bet__teamA bet__team">
+                </div>
+
+                <div className="bet__teamB bet__team">
+                </div>
+
+                <div className="bet__stats">
+                </div>
+            </div>
+        );
+    }
 }
